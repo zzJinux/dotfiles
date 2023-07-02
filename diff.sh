6951a0c7ae6a3126c65a7def91e7caaa33ac31c6
Jinwook Jeong <jinwookjeongg@gmail.com>
2023-07-03 00:54:01 +0900

    fix diff.sh

--- diff.sh
+++ diff.sh
@@ -8,4 +8,4 @@ src=.staging
 dest=$HOME
 
 find "$src" -type f -exec bash -c '! cmp --quiet "$3/${1#"$2/"}" "$1"' - '{}' "$src" "$dest" \; -print \
-| fzf --ansi --preview 'f={};'"git diff --color=always --no-index $(quote dest)/\${f#$(quote dest)/} {}"
+| fzf --ansi --preview 'f={};'"git diff --color=always --no-index $(quote "$dest")/\${f#$(quote "$src")/} {}"
