ff3d3212e9311a06d42017d2995eba2b2c8ad3b0
Jinwook Jeong <jinwookjeongg@gmail.com>
2023-07-22 00:16:20 +0900

    kube portforwarding tool

--- kubernetes/completion.bash
+++ kubernetes/completion.bash
@@ -10,3 +10,16 @@ _fzf_complete_kx() {
   fi < <(find -L ~/.kube/clusters -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;)
 }
 complete -F _fzf_complete_kx kx
+
+_fzf_complete_kpf() {
+  if _fzf_complete --multi --reverse --prompt="target> " -- "$@"; then
+    return
+  elif [ "${#COMP_WORDS[@]}" == "2" ]; then
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local IFS=''
+    read -rd '' wordlist
+    IFS=$'\n'
+    COMPREPLY=( $(compgen -W "$wordlist" -- "$cur") )
+  fi < <(builtin cd ~/.kube/portforwards && find -L . -type f | cut -c3-)
+}
+complete -F _fzf_complete_kpf kpf
