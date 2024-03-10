558ddfe374a2208c0541c7e1ae725e3d0882de37
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-03-10 22:39:49 +0900

    vim: specific TERM required

--- macos/macvim_post.sh
+++ macos/macvim_post.sh
@@ -8,7 +8,15 @@ fi
 
 # MacVim executables
 mkdir -p ~/.local/bin
-ln -fs "$app_bundle/Contents/bin/"* ~/.local/bin/
+for path in "$app_bundle/Contents/bin/"*; do
+  new_path=~/.local/bin/"$(basename "$path")"
+  rm -f "$new_path"
+  cat >"$new_path" <<EOF
+#!/bin/sh
+TERM=xterm-256color exec "$path" "\$@"
+EOF
+  chmod +x "$new_path"
+done
 
 # MacVim manpages
 mkdir -p ~/.local/share/man/man1
