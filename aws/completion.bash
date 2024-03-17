e8c09d7403f8df441fcb037e5d8af9267d0fa064
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-03-17 21:17:04 +0900

    zsh aws completion

--- aws/completion.bash
+++ aws/completion.bash
@@ -1,12 +1,3 @@
-_fzf_complete_awsp() {
-  if _fzf_complete --multi --reverse --prompt="profile> " -- "$@"; then
-    return
-  elif [ "${#COMP_WORDS[@]}" == "2" ]; then
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    local IFS=''
-    read -rd '' wordlist
-    IFS=$'\n'
-    COMPREPLY=( $(compgen -W "$wordlist" -- "$cur") )
-  fi < <(COMP_LINE='aws --profile ' COMP_POINT=14 aws_completer)
-}
+#compdef awsp
+
 complete -F _fzf_complete_awsp awsp
