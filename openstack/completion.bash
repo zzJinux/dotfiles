7c9040eecef49d26023a46be57adbb36f855d0f8
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-03-18 16:51:58 +0900

    openstack: fix zsh comp

--- openstack/completion.bash
+++ openstack/completion.bash
@@ -1,19 +1,3 @@
-_fzf_complete_osc() {
-  local cfg
-  cfg=$(_openstack_locate_config)
-  if [ -z "$cfg" ]; then
-    return 1
-  fi
+#compdef osc osce
 
-  if _fzf_complete --multi --reverse --prompt="cloud> " -- "$@"; then
-    return
-  elif [ "${#COMP_WORDS[@]}" == "2" ]; then
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    local IFS=''
-    read -rd '' wordlist
-    IFS=$'\n'
-    # shellcheck disable=SC2207
-    COMPREPLY=( $(compgen -W "$wordlist" -- "$cur") )
-  fi < <(yq '.clouds|keys|.[]' "$cfg")
-}
 complete -F _fzf_complete_osc osc osce
