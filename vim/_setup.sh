1693fed2bad353f5aa3e19432cc0392e17fc0432
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-09-02 22:25:18 +0900

    typo

--- vim/_setup.sh
+++ vim/_setup.sh
@@ -7,5 +7,5 @@ ohai 'vim:setup Install vim-plug plugins'
 if ! [ -e ~/.vim/autoload/plug.vim ]; then
   execute curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-execute vim -es -i NONE -c "PlugInstall" -c "qa"
-# execute nvim -es -i NONE -c "PlugInstall" -c "qa"
+execute vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa"
+# execute nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
