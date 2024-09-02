a135c2fe0c4ddabf77ecd60f27c50b2bde4e8177
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-09-03 01:35:14 +0900

    macos: app post installs

---
+++ macos/apps/itsycal/post_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+app=com.mowglii.ItsycalApp.plist
+defaults write $app ClockFormat -string "E yyMMdd HH:mm:ss"
+defaults write $app HideIcon -bool true
+defaults write $app PinItsycal -bool false
+defaults write $app ShowEventDays -int 7
