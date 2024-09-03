1e5f8f0cbde5cb876908441300f60fbd24ea7981
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-09-03 13:43:55 +0900

    itsycal: minimize menubar clock

--- macos/apps/itsycal/post_install.sh
+++ macos/apps/itsycal/post_install.sh
@@ -4,3 +4,6 @@ defaults write $app ClockFormat -string "E yyMMdd HH:mm:ss"
 defaults write $app HideIcon -bool true
 defaults write $app PinItsycal -bool false
 defaults write $app ShowEventDays -int 7
+
+# Minimize the menubar clock
+defaults write com.apple.menuextra.clock IsAnalog -bool true
