41ce43b5825633f46b412b06ed136f3e43c9ffa2
Jinwook Jeong <jinwookjeongg@gmail.com>
2024-10-20 22:43:10 +0900

    global python things

--- python/_setup.sh
+++ python/_setup.sh
@@ -9,25 +9,3 @@ python3 -m venv ~/.local/pytoolkit
 source ~/.local/pytoolkit/bin/activate
 pip install -r "$SCRIPT_DIR/requirements.txt"
 python3 -m override_readline
-
-cat <<'EOF' > ~/.local/bin/python
-#!/bin/sh
-exec ~/.local/pytoolkit/bin/python "$@"
-EOF
-
-cat <<'EOF' > ~/.local/bin/python3
-#!/bin/sh
-exec ~/.local/pytoolkit/bin/python3 "$@"
-EOF
-
-cat <<'EOF' > ~/.local/bin/pip
-#!/bin/sh
-exec ~/.local/pytoolkit/bin/pip "$@"
-EOF
-
-cat <<'EOF' > ~/.local/bin/pip3
-#!/bin/sh
-exec ~/.local/pytoolkit/bin/pip3 "$@"
-EOF
-
-chmod +x ~/.local/bin/{python,python3,pip,pip3}
