1f66eda299322cc97b605f441bdd047277a87ce4
Jinwook Jeong <jinwookjeongg@gmail.com>
2025-01-23 23:11:51 +0900

    python downsize

--- python/_setup.sh
+++ python/_setup.sh
@@ -4,18 +4,18 @@ set -eu
 source "$DOTFILES/util.bash"
 SCRIPT_DIR="$(_script_dir)"
 
-ohai 'python:setup Install local Python venv'
-python3 -m venv ~/.local/pytoolkit
-source ~/.local/pytoolkit/bin/activate
-pip install -r "$SCRIPT_DIR/requirements.txt"
-python3 -m override_readline
-deactivate
-
 ohai 'python:install pipx packages'
 while IFS= read -r package; do
   ohai 'python:pipx:install '"$package"
   pipx install "$package"
 done < "$SCRIPT_DIR/pipx_packages.txt"
+
+ohai 'python:setup Install local Python venv'
+virtualenv ~/.local/pytoolkit
+source ~/.local/pytoolkit/bin/activate
+pip install -r "$SCRIPT_DIR/requirements.txt"
+python3 -m override_readline
+deactivate
 
 ohai 'python:link ipython config'
 mkdir -p ~/.ipython/profile_default
